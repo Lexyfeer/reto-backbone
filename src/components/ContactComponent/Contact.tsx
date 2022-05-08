62b2508b5cc749b7b650f8fc9a432b78c49bf6f8
Nancy Ma Rodriguez Lopez <lexyfeer@hotmail.com>
2022-05-08 00:15:50 -0500

    Validations for emails and phone

--- src/components/ContactComponent/Contact.tsx
+++ src/components/ContactComponent/Contact.tsx
@@ -16,11 +16,15 @@ const Contact = ({ }) => {
   const [lastName, setLastName] = useState<string | null>('');
   const [email, setEmail] = useState<string | null>('');
   const [phone, setPhone] = useState<string | null>('');
+  const [exist, setExist] = useState<boolean>(false);
+  const [existPhone, setExistPhone] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [errorPhoneMessage, setErrorPhoneMessage] = useState<string>('');
 
   useEffect(() => {
     const setDefaultData = async () => {
       if (idContact) {
-        await axios.get('https://bkbnchallenge.herokuapp.com/contacts/'+idContact)
+        await axios.get('https://bkbnchallenge.herokuapp.com/contacts/' + idContact)
           .then((response: any) => {
             setFirstName(response.data.firstName)
             setLastName(response.data.lastName)
@@ -32,60 +36,76 @@ const Contact = ({ }) => {
     setDefaultData();
   }, []);
 
-  const updateAPIData = async () => {
+  const modifyContact = async () => {
     if (idContact) {
-      await axios.put('https://bkbnchallenge.herokuapp.com/contacts/'+idContact, { firstName, lastName, email, phone })
-      .then(() => {
-        history('/')
-      })
+      await axios.put('https://bkbnchallenge.herokuapp.com/contacts/' + idContact, { firstName, lastName, email, phone })
+        .then(() => {
+          history('/')
+        })
     }
-    else{
+    else {
       await axios.post('https://bkbnchallenge.herokuapp.com/contacts', { firstName, lastName, email, phone })
-      .then(() => {
-        history('/')
-      })
+        .then(() => {
+          history('/')
+        })
+        .catch((error) => {
+          console.log(error)
+          if(error.response.data.message.includes('email')){
+            setExist(true);
+            setErrorMessage(error.response.data.message)
+          }
+          else{
+            setExistPhone(true);
+            setErrorPhoneMessage(error.response.data.message)
+          }
+        })
     }
   }
 
   return (
     <>
-      <h1>Formulario para { idContact ? <span>actualizar</span> : <span>agregar</span> } contacto</h1>
+      <h1>Formulario para {idContact ? <span>actualizar</span> : <span>agregar</span>} contacto</h1>
 
-      <form className='formContainer' onSubmit={handleSubmit(updateAPIData)}>
+      <form className='formContainer' onSubmit={handleSubmit(modifyContact)}>
         <TextField
+          id='firstName'
+          required
           value={firstName}
           label="First Name"
           margin="dense"
           color="secondary"
-          onChange={(e) => setFirstName(e.target.value)}
-        />
+          onChange={(e) => setFirstName(e.target.value)} />
         <TextField
+          id='lastName'
+          required
           value={lastName}
           label="Last Name"
           margin="dense"
           color="secondary"
-          onChange={(e) => setLastName(e.target.value)}
-        />
+          onChange={(e) => setLastName(e.target.value)} />
         <TextField
+          id='email'
+          type='email'
+          required
+          error={exist}
           value={email}
           label="Email"
           margin="dense"
           color="secondary"
-          onChange={(e) => {
-            setEmail(e.target.value);
-            // pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i
-          }}
-        />
+          onChange={(e) => setEmail(e.target.value)}
+          helperText={errorMessage} />
         <TextField
+          id='phone'
+          required
+          error={existPhone}
           value={phone}
-          
           label="Phone"
           margin="dense"
           color="secondary"
-          onChange={(e) => setPhone(e.target.value)}
-        />
+          onChange={(e) => {setPhone(e.target.value)}}
+          helperText={errorPhoneMessage} />
         <Button type="submit" variant="outlined" >
-          { idContact ? <>Actualizar <EditIcon /></> : <>Agregar <AddIcon /></> }
+          {idContact ? <>Actualizar <EditIcon /></> : <>Agregar <AddIcon /></>}
         </Button>
       </form>
     </>
