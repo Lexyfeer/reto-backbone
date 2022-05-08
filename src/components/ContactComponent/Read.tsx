c8de812fd6d0b43fc14dbab9e52a4b9231493d7e
Nancy Ma Rodriguez Lopez <lexyfeer@hotmail.com>
2022-05-08 14:49:54 -0500

    github pages works test 6

--- src/components/ContactComponent/Read.tsx
+++ src/components/ContactComponent/Read.tsx
@@ -29,12 +29,7 @@ const Read = () => {
   useEffect(() => {
     const getAllContacts = async () => {
       try {
-        const options = { headers: 
-          { 
-            "Permissions-Policy": "interest-cohort=()"
-          }
-        }
-        const { data } = await axios.get('https://bkbnchallenge.herokuapp.com/contacts?perPage=100', options);
+        const { data } = await axios.get('https://bkbnchallenge.herokuapp.com/contacts?perPage=100');
         // https://bkbnchallenge.herokuapp.com/contacts?perPage=100
         setAPIData(data?.results);
       } catch (e) {
